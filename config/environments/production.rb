fadd63a84164803bfb0290e3763b761bdedda489
rakesh kumar <rakeshmaurya262@gmail.com>
2017-07-05 20:29:45 +0530

    smtpl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
      config.action_mailer.default_url_options = { :host => "yourcube.co.uk" }
    config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                  2525,
+    port:                  587,
     domain:               'gmail.com',
     user_name:            ENV["USER_NAME"],
     password:             ENV["PASSWORD"],
