2e438afc2a4682e2ec9f3caf06fedbb4f0d46f95
Yaroslav <rubyracer05@gmail.com>
2017-07-06 15:16:01 +0300

    update email settings

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "chris.forbes@yourcube.co.uk"
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
@@ -30,7 +30,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.serve_static_assets = true
-  
+
   config.assets.compile = true
 
   # config.assets.compile = false
@@ -74,15 +74,17 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-     config.action_mailer.default_url_options = { :host => "yourcube.co.uk" }
-   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                  587,
-    domain:               'gmail.com',
-    user_name:            'fredtig60@gmail.com',
-    password:             'testing60',
-    authentication:       'plain',
-    enable_starttls_auto: true }
+  config.action_mailer.default_url_options = {:host => "yourcube.co.uk"}
+
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.123-reg.co.uk',
+      port: 465,
+      domain: 'yourcube.co.uk',
+      user_name: 'chris.forbes@yourcube.co.uk',
+      password: 'Kenmore1!',
+      authentication: 'plain',
+      ssl: true
+  }
 
 
   # Send deprecation notices to registered listeners.
