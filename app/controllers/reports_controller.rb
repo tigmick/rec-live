b7fb8d309402228fda9ef4860e0999c88a8d1ed6
Ratnam Yadav <ratnam.yadav@icloud.com>
2018-01-18 00:11:05 +0530

    small fix

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,19 +8,12 @@ class ReportsController < ApplicationController
   def get_data
     start_date = params[:start_date].present? ? Date.parse(params[:start_date]) : 1.month.ago.to_date
     end_date = params[:end_date].present? ? Date.parse(params[:end_date]) : Date.today
-    total_open_job_count = current_user.jobs.where('created_at < ?', start_date).count
-    total_closed_job_count = current_user.jobs.where('status = ? and closed_at < ?' ,1 ,start_date).count
     json = {open_jobs: [], closed_jobs: []}
     (start_date..end_date).each do |date|
-      open_job_count = current_user.jobs.where(created_at: date.beginning_of_day..date.end_of_day).count
-      closed_job_count = current_user.jobs.where(status: 1, closed_at: date.beginning_of_day..date.end_of_day).count
-      total_closed_job_count += closed_job_count
-      if date == start_date
-        json[:open_jobs] << [date.strftime('%e %B %Y'), total_open_job_count]
-      else
-        json[:open_jobs] << [date.strftime('%e %B %Y'), open_job_count]
-      end
-      json[:closed_jobs] << [date.strftime('%e %B %Y'), total_closed_job_count]
+      open_job_count = current_user.jobs.where('created_at < ?', date.end_of_day).count
+      closed_job_count = current_user.jobs.where('status = ? and closed_at <= ?' ,1 ,date.end_of_day).count
+      json[:open_jobs] << [date.strftime('%e %B %Y'), open_job_count]
+      json[:closed_jobs] << [date.strftime('%e %B %Y'), closed_job_count]
     end
 
     render json: json
